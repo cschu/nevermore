cschu/nevermore

commit cc764843a3514c72f88e56dcda7003ac39db56da

version -> 0.15.13
- sortmerna: use an existing index subdir of database location, otherwise build the index
- prepare_fastqs:
  - relaxed output name regex
  - attach suffix to sample ids if supplied
- replace tuple() with []
- nevermore_pack: preserve sample.library_source information

nevermore/workflows/input.nf
@@ -42,8 +42,8 @@ process prepare_fastqs {
 	input:
 		tuple val(sample), path(files), val(remote_input), val(library_suffix)
 	output:
-		tuple val(sample), path("fastq/${sample}/${sample}*.fastq.{gz,bz2}"), emit: pairs, optional: true
-		tuple val(sample), path("fastq/${sample}.singles/${sample}*.fastq.{gz,bz2}"), emit: singles, optional: true
+		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: pairs, optional: true
+		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: singles, optional: true
 		path("sample_library_info.txt"), emit: library_info
 
   script:
@@ -106,13 +106,16 @@ workflow fastq_input {
 
 		} else {
 			fastq_ch = fastq_ch
-				.map { file -> return tuple(file.getParent().getName(), file) }
+				.map { file -> [ file.getParent().getName(), file ] }
 		}
 
 		fastq_ch = fastq_ch
 			.groupTuple(by: 0)
 			.combine(libsfx)
-			.map { sample_id, files, suffix -> return tuple(sample_id, files, (params.remote_input_dir != null || params.remote_input_dir), suffix) }
+			.map { sample_id, files, suffix -> 
+				return [ 
+					((suffix == null) ? sample_id : "${sample_id}.${suffix}"), files, (params.remote_input_dir != null || params.remote_input_dir), null ]
+			}
 
 		if (params.ignore_samples) {
 			ignore_samples = params.ignore_samples.split(",")
@@ -163,7 +166,7 @@ workflow bam_input {
 		bam_ch = bam_ch
 			.map { file ->
 				def sample = file.name.replaceAll(bam_suffix_pattern, "").replaceAll(/\.$/, "")
-				return tuple(sample, file)
+				return [ sample, file ]
 			}
 			.groupTuple(sort: true)
 			.map { classify_sample(it[0], it[1]) }

nevermore/modules/decon/sortmerna.nf
@@ -44,11 +44,28 @@ process sortmerna {
 		// 	mv_output = "mv -v work/out/other.fq.gz no_rrna/${sample.id}/${sample.id}_R1.fastq.gz"
 		// }
 
+		// --workdir work/ --idx-dir \$(dirname \$(readlink ${db}))/index/
+		// def index_dir = ""
+		// def prep_index_dir = ""
+		// def rm_index_dir = ""
+		// if (params.sortmerna_db_location_writeable) {
+		// 	prep_index_dir = "ln -s "
+		// }
+
 		"""
-		mkdir -p work/index/ no_rrna/${sample.id}/ rrna/${sample.id}/
-		sortmerna --fastx --aligned --other --threads ${task.cpus} -m ${mem_mb} --workdir work/ --idx-dir \$(dirname \$(readlink ${db}))/index/ ${reads} ${pe_params} --ref ${db}
+		mkdir -p work/ no_rrna/${sample.id}/ rrna/${sample.id}/
+
+		if [[ -d \$(dirname \$(readlink ${db}))/index/ ]]; then 
+			ln -sf \$(dirname \$(readlink ${db}))/index work/
+		else
+			mkdir -p work/index/
+		fi
+
+
+		sortmerna --fastx --aligned --other --threads ${task.cpus} -m ${mem_mb} --workdir work/ --idx-dir work/index ${reads} ${pe_params} --ref ${db}
 
 		${mv_output}
+		rm -rfv work
 		"""
 
 }

nevermore/workflows/align.nf
@@ -48,7 +48,7 @@ workflow nevermore_align {
 			minimap_aligned_ch = minimap2_align.out.sam
 			.map { sample, sam ->
 				sample_id = sample.id.replaceAll(/.(orphans|singles|chimeras)$/, "")
-				return tuple(sample_id, sam)
+				return [ sample_id, sam ]
 			}
 			.groupTuple(sort: true)
 
@@ -57,7 +57,7 @@ workflow nevermore_align {
 				.map { sample_id, samfiles ->
 					def meta = [:]
 					meta.id = sample_id
-					return tuple(meta, samfiles)
+					return [ meta, samfiles ]
 			})
 
 			alignment_ch = alignment_ch
@@ -79,7 +79,7 @@ workflow nevermore_align {
 			aligned_ch = bwa_mem_align.out.bam
 				.map { sample, bam ->
 					sample_id = sample.id.replaceAll(/.(orphans|singles|chimeras)$/, "")
-					return tuple(sample_id, bam)
+					return [ sample_id, bam ]
 				}
 				.groupTuple(sort: true)
 
@@ -87,7 +87,7 @@ workflow nevermore_align {
 				.map { sample_id, bamfiles ->
 					def meta = [:]
 					meta.id = sample_id
-					return tuple(meta, bamfiles)
+					return [ meta, bamfiles ]
 				}, true)
 
 			alignment_ch = alignment_ch

nevermore/workflows/gffquant.nf
@@ -34,7 +34,7 @@ workflow gffquant_flow {
 					.replaceAll(/\.pd\.txt$/, "")
 					.replaceAll(/\.txt\.gz$/, "")
 					.replaceAll(/.+\./, "")
-				return tuple(category, file)
+				return [ category, file ]
 			}
 			.groupTuple(sort: true)
 			.combine(

nevermore/workflows/pack.nf
@@ -14,7 +14,7 @@ workflow nevermore_pack_reads {
 			.map { sample, fastqs ->
 			def meta = sample.clone()
 			meta.is_paired = [fastqs].flatten().size() == 2
-			return tuple(meta, fastqs)
+			return  [ meta, fastqs ]
 		}
 
 		fastq_ch.dump(pretty: true, tag: "pack_fastq_ch")
@@ -27,7 +27,7 @@ workflow nevermore_pack_reads {
 				def meta = sample.clone()
 				meta.id = fastq.name.replaceAll(/_R1.fastq.gz$/, "")
 				meta.merged = false
-				return tuple(meta, fastq)
+				return [ meta, fastq ]
 			}
 
 		/*	route all paired-end read files into a common channel */
@@ -37,7 +37,7 @@ workflow nevermore_pack_reads {
 			.map { sample, fastq ->
 				def meta = sample.clone()
 				meta.merged = true
-				return tuple(meta, fastq)
+				return [ meta, fastq ]
 			}
 
 		/*	group all single-read files by sample and route into merge-channel */
@@ -47,7 +47,7 @@ workflow nevermore_pack_reads {
 				sample, fastq ->
 					def meta = sample.clone()
 					meta.id = sample.id.replaceAll(/.(orphans|singles|chimeras)$/, ".singles")
-					return tuple(meta, fastq)
+					return [ meta, fastq ]
 			}
 			.branch {
 				single_end: it[0].library == "single"
@@ -66,13 +66,17 @@ workflow nevermore_pack_reads {
 			.set { pe_singles_ch }
 
 		merged_single_ch = pe_singles_ch.do_merge
+			// .map { meta, fastq -> [ meta.id, meta.library_source, fastq ] }
 			.map { meta, fastq -> [ meta.id, fastq ] }
+			// .groupTuple(by: [0, 1], sort: true, size: se_group_size, remainder: true)
 			.groupTuple(by: 0, sort: true, size: se_group_size, remainder: true)
+			// .map { sample_id, library_source, fastqs ->
 			.map { sample_id, fastqs ->
 				def meta = [:]
 				meta.id = sample_id
 				meta.is_paired = false
 				meta.library = "paired"
+				meta.library_source = (sample_id.endsWith(".metaT") ? "metaT" : ((sample_id.endsWith(".metaG")) ? "metaG": null)) //library_source
 				meta.merged = true
 				meta.multilib = true
 
@@ -104,7 +108,7 @@ workflow nevermore_pack_reads {
 				def meta = sample.clone()
 				meta.id = fastq.name.replaceAll(/_R1.fastq.gz$/, "")
 				meta.merged = false
-				return tuple(meta, fastq)
+				return [ meta, fastq ]
 			}
 			.mix(pe_singles_ch.no_merge)         // raw PE library orphans
 			.mix(single_reads_ch.single_end)     // SE library reads

nevermore/workflows/prep.nf
@@ -94,16 +94,14 @@ workflow nevermore_simple_preprocessing {
 				calculate_library_size_cutoff.out.library_sizes.view()
 
 				css_ch = check_subsample_ch.subsample
-					.map { sample, fastqs -> return tuple(sample.id, sample, fastqs) }
+					.map { sample, fastqs ->  [ sample.id, sample, fastqs ] }
 					.join(
 						
 						calculate_library_size_cutoff.out.library_sizes
 							.splitCsv(header: true, sep: '\t', strip: true)
-							.map { row ->
-								return tuple(row.sample, row.do_subsample == "1", row.target_size)
-							},
-							by: 0,
-							remainder: true						
+							.map { row -> [ row.sample, row.do_subsample == "1", row.target_size ] },
+						by: 0,
+						remainder: true						
 					)
 
 				css_ch.dump(pretty: true, tag: "css_ch")
@@ -112,18 +110,14 @@ workflow nevermore_simple_preprocessing {
 				// for some reason, .branch does not work here :S
 				subsample_ch = css_ch
 					.filter { it[3] }
-					.map { sample_id, sample, fastqs, do_subsample, target_size ->
-						return tuple(sample, fastqs, target_size)
-					}
+					.map { sample_id, sample, fastqs, do_subsample, target_size -> [ sample, fastqs, target_size ] }
 				subsample_ch.dump(pretty: true, tag: "subsample_ch")
 
 				subsample_reads(subsample_ch)
 
 				do_not_subsample_ch = css_ch
 					.filter { !it[3] }
-					.map { sample_id, sample, fastqs, do_subsample, target_size ->
-						return tuple(sample, fastqs)
-					}
+					.map { sample_id, sample, fastqs, do_subsample, target_size -> [ sample, fastqs ] }
 					.mix(
 						check_subsample_ch.no_subsample
 					)
@@ -155,7 +149,7 @@ workflow nevermore_simple_preprocessing {
 					def meta = sample.clone()
 					meta.id = sample.id + ".orphans"
 					meta.is_paired = false
-					return tuple(meta, file)
+					return [ meta, file ]
 				}
 
 		}

commit 8ae6e0e48f66292d35b784e585e2209424606af7

version -> 0.15.14; fix singles input issue introduced in cc76484

nevermore/workflows/input.nf
@@ -43,7 +43,7 @@ process prepare_fastqs {
 		tuple val(sample), path(files), val(remote_input), val(library_suffix)
 	output:
 		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: pairs, optional: true
-		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: singles, optional: true
+		tuple val(sample), path("fastq/**singles_R1.fastq.{gz,bz2}"), emit: singles, optional: true
 		path("sample_library_info.txt"), emit: library_info
 
   script:

nextflow.config
@@ -4,7 +4,7 @@ manifest {
 	description = "Metaomics pipeline toolbox"
 	name = "nevermore"
 	nextflowVersion = ">=22.10.6"
-	version = "0.15.11"
+	version = "0.15.14"
 }
 
 params {