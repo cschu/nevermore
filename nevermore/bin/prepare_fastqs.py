cschu/nevermore

commit 8ae6e0e48f66292d35b784e585e2209424606af7

version -> 0.15.14; fix singles input issue introduced in cc76484

nevermore/workflows/input.nf
@@ -43,7 +43,7 @@ process prepare_fastqs {
 		tuple val(sample), path(files), val(remote_input), val(library_suffix)
 	output:
 		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: pairs, optional: true
-		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: singles, optional: true
+		tuple val(sample), path("fastq/**singles_R1.fastq.{gz,bz2}"), emit: singles, optional: true
 		path("sample_library_info.txt"), emit: library_info
 
   script:

nextflow.config
@@ -4,7 +4,7 @@ manifest {
 	description = "Metaomics pipeline toolbox"
 	name = "nevermore"
 	nextflowVersion = ">=22.10.6"
-	version = "0.15.11"
+	version = "0.15.14"
 }
 
 params {

commit 1337f960e32ea801f6c0728a7b32f8849eb60323

version -> 0.15.16 - fixed input issue caused in 8ae6e0e (0.15.14) and cc76484 (0.15.13) - add multi-suffix support to prepare_fastqs.py - add sentinel to sortmerna process - add sample_id-tags to various processes

nevermore/bin/prepare_fastqs.py
@@ -112,7 +112,7 @@ def transfer_multifiles(files, dest, remote_input=False, compression=None):
 def process_sample(
 	sample, fastqs, output_dir,
 	fastq_suffix_pattern,
-	remove_suffix=None, remote_input=False,
+	remove_suffix=[], remote_input=False,
 	add_suffix=None,
 ):
 	""" Checks if a set of fastq files in a directory is a valid collection
@@ -177,7 +177,10 @@ def process_sample(
 		prefixes = [re.sub(fastq_suffix_pattern, "", os.path.basename(f)) for f in fastqs]
 		if remove_suffix:
 			# remove suffix pattern if requested
-			prefixes = [re.sub(remove_suffix + r"$", "", p) for p in prefixes]
+			pattern = r"({})".format("|".join(remove_suffix)) + r'$'
+
+			# prefixes = [re.sub(remove_suffix + r"$", "", p) for p in prefixes]
+			prefixes = [re.sub(pattern, "", p) for p in prefixes]
 
 		print("PRE", prefixes, file=sys.stderr)
 
@@ -366,7 +369,8 @@ def main():
 				renamed = process_sample(
 					sample, fastqs, args.output_dir,
 					fastq_file_suffix_pattern,
-					remove_suffix=args.remove_suffix, remote_input=args.remote_input,
+					remove_suffix=args.remove_suffix.split(",") if args.remove_suffix else [],
+					remote_input=args.remote_input,
 					add_suffix=args.add_sample_suffix,
 				)
 			except Exception as e:

nevermore/modules/decon/sortmerna.nf
@@ -1,6 +1,7 @@
 process sortmerna {
 	container "quay.io/biocontainers/sortmerna:4.3.6--h9ee0642_0"
 	label "medium"
+	tag "${sample.id}"
 
 	input:
 		tuple val(sample), path(fastqs)
@@ -8,6 +9,7 @@ process sortmerna {
 	output:
 		tuple val(sample), path("no_rrna/${sample.id}/*.fastq.gz"), emit: fastqs, optional: true
 		tuple val(sample), path("rrna/${sample.id}/*.fastq.gz"), emit: rrna_fastqs, optional: true
+		tuple val(sample), path("${sample.id}.SORTMERNA_DONE"), emit: done_sentinel
 	script:
 		def mem_mb = task.memory.toMega()
 
@@ -53,6 +55,8 @@ process sortmerna {
 		// }
 
 		"""
+		set -e -o pipefail
+
 		mkdir -p work/ no_rrna/${sample.id}/ rrna/${sample.id}/
 
 		if [[ -d \$(dirname \$(readlink ${db}))/index/ ]]; then 
@@ -66,6 +70,8 @@ process sortmerna {
 
 		${mv_output}
 		rm -rfv work
+
+		touch ${sample.id}.SORTMERNA_DONE
 		"""
 
 }

nevermore/workflows/input.nf
@@ -34,16 +34,15 @@ process transfer_bams {
 
 
 process prepare_fastqs {
-	// container "ghcr.io/astral-sh/uv:python3.14-trixie-slim"
-	// container "registry.git.embl.org/schudoma/portraits_metatraits:latest"
 	container "quay.io/biocontainers/pandas:2.2.1"
 	label "default"
+	tag "${sample}"
 
 	input:
 		tuple val(sample), path(files), val(remote_input), val(library_suffix)
 	output:
-		tuple val(sample), path("fastq/**.fastq.{gz,bz2}"), emit: pairs, optional: true
-		tuple val(sample), path("fastq/**singles_R1.fastq.{gz,bz2}"), emit: singles, optional: true
+		tuple val(sample), path("fastq/${sample}/*.fastq.{gz,bz2}"), emit: pairs, optional: true
+		tuple val(sample), path("fastq/${sample}.singles/*.fastq.{gz,bz2}"), emit: singles, optional: true
 		path("sample_library_info.txt"), emit: library_info
 
   script:

nextflow.config
@@ -4,7 +4,7 @@ manifest {
 	description = "Metaomics pipeline toolbox"
 	name = "nevermore"
 	nextflowVersion = ">=22.10.6"
-	version = "0.15.14"
+	version = "0.15.16"
 }
 
 params {